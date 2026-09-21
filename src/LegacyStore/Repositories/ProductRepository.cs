f437ce3935aa9fb278921bd169a8b99d1a7833d3
nlngoclong-djade <nlngoclong@gmail.com>
2026-09-21 23:34:17 +0700

    task 2 complete

--- src/LegacyStore/Repositories/ProductRepository.cs
+++ src/LegacyStore/Repositories/ProductRepository.cs
@@ -1,11 +1,14 @@
 using LegacyStore.DataStore.Interfaces;
+using LegacyStore.Helpers;
 using LegacyStore.Models;
 
 namespace LegacyStore.Repositories;
 
 public class ProductRepository
 {
+    int capacity = 10; // get from config but not know how to get it here
     private readonly IProductDataStore _store;
+    private readonly LruCache<string, Product> _lruCache = new LruCache<string, Product>(10);
 
     public ProductRepository(IProductDataStore store)
     {
@@ -14,11 +17,21 @@ public class ProductRepository
 
     public Product? Get(string sku)
     {
-        return _store.Load(sku);
+        /// cached
+        if (_lruCache.TryGet(sku, out var product))
+        {
+            return product;
+        }
+        /// not cached
+        var cacheValue =  _store.Load(sku);
+        _lruCache.Put(sku, cacheValue);
+        return cacheValue;
     }
 
     public void Save(Product product)
     {
+        _lruCache.Remove(product.Sku);
         _store.Save(product);
+        _lruCache.Put(product.Sku, product);
     }
 }
